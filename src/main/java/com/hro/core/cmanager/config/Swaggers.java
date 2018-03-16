87df1dc68e88403354bfab9fd1daa0271994aeb0
Mojianzhang <361754100@qq.com>
2018-03-16 17:33:07 +0800

    修改 Swagger配置

--- src/main/java/com/hro/core/cmanager/config/Swaggers.java
+++ src/main/java/com/hro/core/cmanager/config/Swaggers.java
@@ -1,14 +1,21 @@
 package com.hro.core.cmanager.config;
 
+import com.hro.core.cmanager.common.Constant;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -16,13 +23,18 @@ public class Swaggers {
 
     @Bean
     public Docket createRestApi() {
+        ParameterBuilder tokenPar = new ParameterBuilder();
+        List<Parameter> pars = new ArrayList<Parameter>();
+        tokenPar.name(Constant.X_AUTH_HEADER).description("令牌").modelRef(new ModelRef("string")).parameterType("header").required(false).build();
+        pars.add(tokenPar.build());
+
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.hro.core.cmanager.controller"))
                 .paths(PathSelectors.any())
-//                .paths(PathSelectors.regex("/cmanager/*"))//过滤的接口
-                .build();
+                .build()
+                .globalOperationParameters(pars);
     }
 
     ApiInfo apiInfo() {
@@ -37,5 +49,5 @@ public class Swaggers {
                 .build();
     }
 
-
 }
+
